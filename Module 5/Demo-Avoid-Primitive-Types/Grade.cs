f0f6352579644e5ae8909a035a6d74127faf5fd0
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-06-07 16:22:24 +0100

    Add initial module 5

---
+++ Module 5/Demo-Avoid-Primitive-Types/Grade.cs
@@ -0,0 +1,19 @@
+﻿namespace Demo_Avoid_Primitive_Types
+{
+    public sealed class Grade
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Grade"/> class.
+        /// </summary>
+        private Grade()
+        {
+        }
+
+        public static Grade A { get; } = new Grade();
+        public static Grade B { get; } = new Grade();
+        public static Grade C { get; } = new Grade();
+        public static Grade D { get; } = new Grade();
+        public static Grade E { get; } = new Grade();
+        public static Grade F { get; } = new Grade();
+    }
+}
---
+++ Module 5/Demo-Avoid-Primitive-Types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo_Avoid_Primitive_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
